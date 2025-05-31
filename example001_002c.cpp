55b06989b133b261f2f17823135f8d2ada8d7323
Puni2Samurai <rsh43047@jcom.home.ne.jp>
2025-06-01 08:45:45 +0900

    ダブルクリック対応ウィンドウを生成

---
+++ example001_002c.cpp
@@ -0,0 +1,104 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowFunc(HWND, UINT, WPARAM, LPARAM);
+
+////////////////////////////////////////////////////////////////
+// エントリー関数
+////////////////////////////////////////////////////////////////
+int WINAPI WinMain(HINSTANCE hThisInst, HINSTANCE hPrevInst, LPSTR lpszArgs, int nWinMode)
+{
+    HWND hwnd;
+    MSG msg;
+    WNDCLASSEXA wcl;
+    const char szWinName[] = "ExampleWin";
+    const char szWinTitle[] = "Window Title: Example";
+
+    // ウィンドウクラス定義
+    wcl.hInstance = hThisInst;
+    wcl.lpszClassName = szWinName;
+    wcl.lpfnWndProc = WindowFunc;
+    wcl.style = wcl.style = CS_DBLCLKS;
+    wcl.cbSize = sizeof(WNDCLASSEXA);
+    wcl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcl.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+    wcl.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcl.lpszMenuName = NULL;
+    wcl.cbClsExtra = 0;
+    wcl.cbWndExtra = 0;
+    wcl.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+
+    if(!RegisterClassExA(&wcl))
+    {
+        return 0;
+    }
+
+    // ウィンドウ生成
+    hwnd = CreateWindowA(
+        szWinName,
+        szWinTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        640, 480,
+        HWND_DESKTOP,
+        NULL,
+        hThisInst,
+        NULL
+    );
+
+    ShowWindow(hwnd, nWinMode);
+    UpdateWindow(hwnd);
+
+    // メッセージループ
+    while(TRUE)
+    {
+        if(PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+        {
+            if(!GetMessage(&msg, NULL, 0, 0))
+            {
+                return msg.wParam;
+            }
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            // ここにアプリ固有の処理を記述
+        }
+    }
+
+    return msg.wParam;
+}
+
+
+////////////////////////////////////////////////////////////////
+// ウィンドウコールバック関数
+////////////////////////////////////////////////////////////////
+LRESULT CALLBACK WindowFunc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    int iResp;
+
+    switch(message)
+    {
+        // ダブルクリック(左ボタン)
+        case WM_LBUTTONDBLCLK:
+            iResp = MessageBoxA(hwnd, "Quit this app ?", "Confirm", MB_YESNO);
+            if(iResp == IDYES)
+            {
+                PostQuitMessage(0);
+            } 
+            break;
+
+            // プログラム終了
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        // その他メッセージ処理
+        default:
+            return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
