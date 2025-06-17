71645b8ccaef0cc63c479d74dfa39c0505778f4a
Puni2Samurai <rsh43047@jcom.home.ne.jp>
2025-06-17 21:44:56 +0900

    ハンドルを参照渡しに修正、インデントを修正

--- example001_003/CExample.cpp
+++ example001_003/CExample.cpp
@@ -9,10 +9,10 @@ extern LRESULT CALLBACK WindowFunc(HWND, UINT, WPARAM, LPARAM);
 // 初期化処理
 BOOL CMyExample::CreateExampleWindow(HINSTANCE hAppInst, int nShowCmd)
 {
-	hThisInst = hAppInst;
-	nWinMode = nShowCmd;
+    hThisInst = hAppInst;
+    nWinMode = nShowCmd;
 
-	WNDCLASSEXA wcl;
+    WNDCLASSEXA wcl;
     const char szWinName[] = "ExampleWin";
     const char szWinTitle[] = "Example: Device Independent Bitmap";
 
@@ -36,24 +36,24 @@ BOOL CMyExample::CreateExampleWindow(HINSTANCE hAppInst, int nShowCmd)
     }
 
     // ウィンドウ表示位置を算出
-	int x, y;
-	DWORD dwStyle;
-	RECT rcAppWin, rcDisplay;
+    int x, y;
+    DWORD dwStyle;
+    RECT rcAppWin, rcDisplay;
 
-	x = y = 0;
-	dwStyle = WS_CAPTION | WS_VISIBLE | WS_POPUP;
-	rcAppWin.left = 0;
-	rcAppWin.top = 0;
-	rcAppWin.right = 640;
-	rcAppWin.bottom = 480;
-	if(AdjustWindowRectEx(&rcAppWin, dwStyle, FALSE, 0) != FALSE)
-	{
-		if(SystemParametersInfo(SPI_GETWORKAREA, 0, &rcDisplay, 0) != FALSE)
-		{
-			x = rcDisplay.left + ((rcDisplay.right - rcDisplay.left) - (rcAppWin.right - rcAppWin.left)) / 2;
-			y = rcDisplay.top  + ((rcDisplay.bottom - rcDisplay.top) - (rcAppWin.bottom - rcAppWin.top)) / 2;
-		}
-	}
+    x = y = 0;
+    dwStyle = WS_CAPTION | WS_VISIBLE | WS_POPUP;
+    rcAppWin.left = 0;
+    rcAppWin.top = 0;
+    rcAppWin.right = 640;
+    rcAppWin.bottom = 480;
+    if(AdjustWindowRectEx(&rcAppWin, dwStyle, FALSE, 0) != FALSE)
+    {
+        if(SystemParametersInfo(SPI_GETWORKAREA, 0, &rcDisplay, 0) != FALSE)
+        {
+            x = rcDisplay.left + ((rcDisplay.right - rcDisplay.left) - (rcAppWin.right - rcAppWin.left)) / 2;
+            y = rcDisplay.top  + ((rcDisplay.bottom - rcDisplay.top) - (rcAppWin.bottom - rcAppWin.top)) / 2;
+        }
+    }
 
     // ウィンドウ生成
     hwnd = CreateWindowA(
@@ -72,38 +72,38 @@ BOOL CMyExample::CreateExampleWindow(HINSTANCE hAppInst, int nShowCmd)
     UpdateWindow(hwnd);
     SetFocus(hwnd);
 
-	return TRUE;
+    return TRUE;
 }
 
 ////////////////////////////////////////////////////////////////
 // 初期化処理
 BOOL CMyExample::InitExample()
 {
-	// DIBクラス初期化
-	pDibSprite = new CDib24(hwnd);
-	if(pDibSprite == NULL)
-	{
-		return FALSE;
-	}
+    // DIBクラス初期化
+    pDibSprite = new CDib24(hwnd);
+    if(pDibSprite == NULL)
+    {
+        return FALSE;
+    }
 
-	// BMPファイル読み込み
-	if(pDibSprite->ReadBMPFile("example001_003.bmp") == FALSE)
-	{
-		return FALSE;
-	}
+    // BMPファイル読み込み
+    if(pDibSprite->ReadBMPFile("example001_003.bmp") == FALSE)
+    {
+        return FALSE;
+    }
 
-	InvalidateRect(hwnd, NULL, FALSE);
+    InvalidateRect(hwnd, NULL, FALSE);
 
-	return TRUE;
+    return TRUE;
 }
 
 ////////////////////////////////////////////////////////////////
 // 描画内容の更新
 void CMyExample::UpdateScreen(HDC hDC)
 {
-	if(pDibSprite != NULL)
-	{
-		pDibSprite->DrawBits(hDC, 0, 0);
-	}
+    if(pDibSprite != NULL)
+    {
+        pDibSprite->DrawBits(hDC, 0, 0);
+    }
 }
 
--- example001_003/CExample.h
+++ example001_003/CExample.h
@@ -7,14 +7,14 @@ class CMyExample
 {
 protected:
     HWND hwnd;
-	HINSTANCE hThisInst;
-	int nWinMode;
+    HINSTANCE hThisInst;
+    int nWinMode;
 
-	CDib24 *pDibSprite;
+    CDib24 *pDibSprite;
 
 public:
-	BOOL CreateExampleWindow(HINSTANCE hAppInst, int nShowCmd);
-	BOOL InitExample();
-	void UpdateScreen(HDC hDC);
+    BOOL CreateExampleWindow(HINSTANCE hAppInst, int nShowCmd);
+    BOOL InitExample();
+    void UpdateScreen(HDC hDC);
 };
 
